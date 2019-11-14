8d98634bceed10087eb94d3f31ed028e1e105aa9
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2019-11-14 10:05:30 +0100

    search scroll query refacto

--- worker-paginator-calc/src/main/kotlin/elasticsearchpaginator/workerpaginatorcalc/repository/EntityElasticsearchRepository.kt
+++ worker-paginator-calc/src/main/kotlin/elasticsearchpaginator/workerpaginatorcalc/repository/EntityElasticsearchRepository.kt
@@ -1,10 +1,7 @@
 package elasticsearchpaginator.workerpaginatorcalc.repository
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import elasticsearchpaginator.core.util.ElasticsearchUtils.async
 import elasticsearchpaginator.workerpaginatorcalc.configuration.ElasticsearchProperties
-import elasticsearchpaginator.workerpaginatorcalc.exception.UnexpectedQueryException
 import org.elasticsearch.action.search.*
 import org.elasticsearch.client.RequestOptions
 import org.elasticsearch.client.RestHighLevelClient
@@ -16,6 +13,7 @@ import org.elasticsearch.common.xcontent.XContentFactory
 import org.elasticsearch.common.xcontent.XContentType
 import org.elasticsearch.search.SearchModule
 import org.elasticsearch.search.builder.SearchSourceBuilder
+import org.elasticsearch.search.sort.FieldSortBuilder
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
 import java.util.*
@@ -23,43 +21,39 @@ import java.util.*
 
 @Repository
 class EntityElasticsearchRepository(private val restHighLevelClient: RestHighLevelClient,
-                                    private val mapper: ObjectMapper,
                                     private val elasticsearchProperties: ElasticsearchProperties) {
 
     private val scrollKeepAlive = TimeValue.timeValueMillis(this.elasticsearchProperties.scrollKeepAliveDuration.toMillis())
 
     fun searchScroll(index: String, query: String, sort: String, size: Int): Mono<SearchResponse> {
-        val includes = this.mapper.readValue<List<Any>>(sort)
-                .map { s ->
-                    when (s) {
-                        is String -> s
-                        is Map<*, *> -> s.keys.first() as String
-                        else -> throw UnexpectedQueryException("Could not read sort value '$s'")
-                    }
-                }
-                .map { fieldName -> fieldName.removeSuffix(".keyword") }
-                .map { fieldName -> """"$fieldName"""" }
-                .joinToString(",", "[", "]")
-
         val payload = """
             {
-                "_source": $includes,
                 "query": $query,
                 "sort": $sort
             }
         """.trimIndent()
 
         return Mono.fromCallable {
-            val searchSourceBuilder = SearchSourceBuilder()
-            val searchModule = SearchModule(Settings.EMPTY, false, Collections.emptyList())
-            val parser = XContentFactory.xContent(XContentType.JSON)
-                    .createParser(
-                            NamedXContentRegistry(searchModule.namedXContents),
-                            DeprecationHandler.THROW_UNSUPPORTED_OPERATION,
-                            payload
-                    )
-            searchSourceBuilder.parseXContent(parser)
-            searchSourceBuilder
+            SearchSourceBuilder()
+                    .apply {
+                        val searchModule = SearchModule(Settings.EMPTY, false, Collections.emptyList())
+                        val parser = XContentFactory.xContent(XContentType.JSON)
+                                .createParser(
+                                        NamedXContentRegistry(searchModule.namedXContents),
+                                        DeprecationHandler.THROW_UNSUPPORTED_OPERATION,
+                                        payload
+                                )
+                        this.parseXContent(parser)
+                    }
+                    .apply {
+                        val includes = this.sorts()
+                                .filterIsInstance<FieldSortBuilder>()
+                                .map { fieldSortBuilder ->
+                                    fieldSortBuilder.fieldName.removeSuffix(".keyword")
+                                }
+                                .toTypedArray()
+                        this.fetchSource(includes, null)
+                    }
         }
                 .map { searchSourceBuilder ->
                     SearchRequest()
